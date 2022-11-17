6dce419488e2d12756b6a93568ba2bc6c6c6fc00
ravilella1234 <masters.selenium@gmail.com>
2022-11-17 10:02:51 +0530

    Constructors Day-2

--- November2022_9AMJavaBatch/src/icici/loans/eduloans/Employee.java
+++ November2022_9AMJavaBatch/src/icici/loans/eduloans/Employee.java
@@ -4,34 +4,54 @@ public class Employee
 {
 	int eno; 
 	String ename;
+	float sal;
 
-	 public Employee()
-	 {
-		 System.out.println("iam default constructor...");
-	 }
-	
-	public Employee(int i, String string) 
+	/*public Employee()
 	{
-		eno = i;
-		ename = string;
+		System.out.println("iam default constructor...");
+		
+		System.out.println(this.eno);
+		System.out.println(this.ename);
+		System.out.println(this.sal);
+	}*/
+
+	public Employee(int eno) 
+	{
+			//this();
+			this.eno = eno;
+			
+			System.out.println(this.eno);
+			System.out.println(this.ename);
+			System.out.println(this.sal);
 	}
-
-	public Employee(int i) 
+	 
+	public Employee(int eno, String ename) 
 	{
-		eno = i;
+		this(eno);
+		this.eno = eno;
+		this.ename = ename;
+		
+		System.out.println(this.eno);
+		System.out.println(this.ename);
+		System.out.println(this.sal);
+	}
+	
+	public Employee(int eno, String ename, float sal) 
+	{
+		this(eno,ename);
+		this.eno = eno;
+		this.ename = ename;
+		this.sal = sal;
+		
+		System.out.println(this.eno);
+		System.out.println(this.ename);
+		System.out.println(this.sal);
 	}
 
 	public static void main(String[] args)
-	{
-		Employee e1 = new Employee();
-		System.out.println(e1.eno);
-		System.out.println(e1.ename);
+	{		
+		Employee e4 = new Employee(1745, "sai", 12.34f);
 		
-		Employee e2 = new Employee(1744,"ravi");
-		System.out.println(e2.eno);
-		System.out.println(e2.ename);
-		
-		Employee e3 = new Employee(1743);
 	}
 	
 }
---
+++ November2022_9AMJavaBatch/src/icici/loans/eduloans/EmployeeChild.java
@@ -0,0 +1,21 @@
+package icici.loans.eduloans;
+
+public class EmployeeChild extends Employee
+{
+	String city;
+
+	public EmployeeChild(String city) 
+	{
+		//super();
+		super(100,"sai",12.34f);
+		this.city = city;
+		
+		System.out.println(this.city);
+	}
+	
+	public static void main(String[] args) 
+	{
+		EmployeeChild ec = new EmployeeChild("hyderabad");
+	}
+
+}
